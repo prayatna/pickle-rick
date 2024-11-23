b65e3c9919a9d384ff30c09a2ea57f61c425ec05
Prayatna Bhattarai <prayatna.bhattarai@gmail.com>
2024-11-23 16:13:00 +0800

    update signup page and persist data to cookie

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,18 +1,60 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import { Field } from "@/components/ui/field";
+import { SIGNUP_DATA_COOKIE_NAME } from "@/constants";
 import { Box, Input, VStack } from "@chakra-ui/react";
+import Cookie from "js-cookie";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
+
+interface SignupData {
+  userName: string;
+  jobTitle: string;
+}
 
 export default function Signup() {
+  const [signupData, setSignupData] = useState<SignupData>({
+    userName: "",
+    jobTitle: "",
+  });
+
+  const router = useRouter();
+
+  function handleClick() {
+    Cookie.set(SIGNUP_DATA_COOKIE_NAME, JSON.stringify(signupData), {
+      expires: 7,
+    });
+
+    router.push("/characters");
+  }
+
   return (
     <Box w={{ mdTo2xl: "xl", base: "sm" }}>
       <VStack gap={"5"}>
         <Field label="Username" width={"100%"}>
-          <Input placeholder="Rick" />
+          <Input
+            placeholder="Rick"
+            value={signupData.userName}
+            onChange={(event) => {
+              signupData.userName = event.target.value;
+              setSignupData({ ...signupData });
+            }}
+          />
         </Field>
         <Field label="Job title">
-          <Input placeholder="Megagenius scientist" />
+          <Input
+            placeholder="Megagenius scientist"
+            value={signupData.jobTitle}
+            onChange={(event) => {
+              signupData.jobTitle = event.target.value;
+              setSignupData({ ...signupData });
+            }}
+          />
         </Field>
-        <Button size="md">Sign Up</Button>
+        <Button size="md" onClick={handleClick}>
+          Sign Up
+        </Button>
       </VStack>
     </Box>
   );
---
+++ src/constants.ts
@@ -0,0 +1,1 @@
+export const SIGNUP_DATA_COOKIE_NAME = "signup-data";
