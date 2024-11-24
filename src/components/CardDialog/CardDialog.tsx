a627bd9d5d4b4b36661f528468c81b179f561bf6
Prayatna Bhattarai <prayatna.bhattarai@gmail.com>
2024-11-24 15:39:38 +0800

    style cardDialog and populate character info

--- src/components/CardDialog/CardDialog.tsx
+++ src/components/CardDialog/CardDialog.tsx
@@ -12,9 +12,9 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
-import { useLazyQuery, useQuery } from "@apollo/client";
+import { useLazyQuery } from "@apollo/client";
 import { CharacterDetail, GET_CHARACTER_DETAILS } from "@/graphql/characters";
-import { Spinner } from "@chakra-ui/react";
+import { Box, Center, Image, Spinner, Text } from "@chakra-ui/react";
 
 export default function CardDialog({ characterId }: { characterId: string }) {
   const [getCharacterDetail, { loading, error, data }] = useLazyQuery(
@@ -61,9 +61,28 @@ function CharacterDetailContent({
   return (
     <DialogContent>
       <DialogHeader>
-        <DialogTitle>{characterDetails.name}</DialogTitle>
+        <DialogTitle textAlign={"center"}>
+          <Text textStyle="5xl">{characterDetails.name}</Text>
+        </DialogTitle>
       </DialogHeader>
       <DialogBody>
+        <Center>
+          <Image src={characterDetails.image} alt={characterDetails.name} />
+        </Center>
+        <Box>
+          <Center>
+            <Text textStyle="xl">Status: {characterDetails.status} </Text>
+          </Center>
+          <Text textStyle="md">Species: {characterDetails.species} </Text>
+          <Text textStyle="md">Gender: {characterDetails.gender} </Text>
+          <Text textStyle="md">
+            Location: {characterDetails.location.name || "Unknown"}
+          </Text>
+          <Text textStyle="md">
+            First Appearance (Episode):
+            {characterDetails.episode[0]?.name || "Unknown"}
+          </Text>
+        </Box>
         <p>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua.
